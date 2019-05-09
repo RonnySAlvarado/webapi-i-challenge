07b27a0bdc4e88c23a76a1f333a5d0685abd223b
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-08 20:01:02 -0500

    Initial commit - set up the boiler plate for backend points with Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+
+const express = require("express");
+
+const server = express();
+const port = 9000;
+
+const db = require("./data/db.js");
+
+server.listen(port, () => console.log(`API running on port ${port}`));
