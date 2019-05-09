e14f00721966a3c1ca60cef41c9c1b8d6fe4ce03
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-08 20:35:32 -0500

    Made a get request to retrieve users in the database

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const express = require("express");
 const server = express();
 const port = 9000;
 
+server.use(express.json());
+
 const db = require("./data/db.js");
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.send(users); // or res.status(200).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved" });
+    });
+});
 
 server.listen(port, () => console.log(`API running on port ${port}`));
